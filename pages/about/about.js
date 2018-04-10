a79c870c1f202a9ddddbcfa5e30ac9ff05dd8ec6
Alan <76496685@qq.com>
2018-04-10 18:07:31 +0800

    增加了关于商家，足迹增加和列表还不完善

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,8 +1,47 @@
 //index.js
 //获取应用实例
+const {relativeurl} = require('../../util')
 const app = getApp()
 
 Page({
-  data: {
-  },
+	data: {},
+	onLoad(){
+		wx.request({
+			method: "GET",
+			url: relativeurl + 'api/wxapp/about',
+			dataType: 'json',
+			data: {},
+			header: {},
+			success(res) {
+				if (res.statusCode == 200) {
+					const {address, contact, desc,name} = res.data.data
+
+				} else {
+					wx.showModal({
+						title: '提示',
+						content: '请求出错',
+						showCancel: false,
+						success: function (res) {
+							if (res.confirm) {
+							}
+						}
+					})
+				}
+			},
+			fail() {
+				wx.showModal({
+					title: '提示',
+					content: '请求出错',
+					showCancel: false,
+					success: function (res) {
+						if (res.confirm) {
+						}
+					}
+				})
+			},
+			complete: function () {
+
+			}
+		})
+	}
 })
--- pages/business/business.js
+++ pages/business/business.js
@@ -1,8 +1,52 @@
 //index.js
 //获取应用实例
-const {calling} = require('../../util')
+const {calling,relativeurl} = require('../../util')
 
 Page({
   data: {},
-  calling
+  calling(){
+	  calling(this.data.pageData.contact.phone[0])
+  },
+	onLoad(){
+        const vm = this;
+		wx.request({
+			method: "GET",
+			url: relativeurl + 'api/wxapp/about',
+			dataType: 'json',
+			data: {},
+			header: {},
+			success(res) {
+				if (res.statusCode == 200) {
+				  vm.setData({
+						pageData:res.data.data
+					});
+
+				} else {
+					wx.showModal({
+						title: '提示',
+						content: '请求出错',
+						showCancel: false,
+						success: function (res) {
+							if (res.confirm) {
+							}
+						}
+					})
+				}
+			},
+			fail() {
+				wx.showModal({
+					title: '提示',
+					content: '请求出错',
+					showCancel: false,
+					success: function (res) {
+						if (res.confirm) {
+						}
+					}
+				})
+			},
+			complete: function () {
+
+			}
+		})
+	}
 })
